5f5279b4e970c0f141a52a63bbcee4775a004fce
DoryBoy <williamodaw@outlook.com>
2022-05-10 15:08:21 +0100

    finish main taks

--- src/com/williamdaw/Main.java
+++ src/com/williamdaw/Main.java
@@ -6,23 +6,30 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner  scanner = new Scanner(system.in);
-        ArrayList<String> numbers = new ArrayList<>();
+        int num;
+        Scanner  scanner = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
-
+            System.out.println("enter number");
+            num = scanner.nextInt();
+            numbers.add(num);
         }
-        int n = arr.length;
+        int n = numbers.size();
         int temp = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 1; j < (n - i); j++) {
-                if (arr[j - 1] > arr[j]) {
+                if (numbers.get((j - 1)) > numbers.get(j)) {
                     //swap elements
-                    temp = arr[j - 1];
-                    arr[j - 1] = arr[j];
-                    arr[j] = temp;
+                    temp = numbers.get((j-1));
+                    numbers.set((j - 1),numbers.get(j));
+                    numbers.set(j,temp);
                 }
 
             }
+        }
+        for (Integer number : numbers) {
+            System.out.println(number);
+
         }
     }
 }
