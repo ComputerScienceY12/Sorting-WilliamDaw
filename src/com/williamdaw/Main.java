6416af528e2c71e2ab015e80ac416bfe09c74515
DoryBoy <williamodaw@outlook.com>
2022-05-10 17:45:10 +0100

    with letters

--- src/com/williamdaw/Main.java
+++ src/com/williamdaw/Main.java
@@ -1,6 +1,7 @@
 package com.williamdaw;
 
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
@@ -8,33 +9,49 @@ public class Main {
     public static void main(String[] args) {
         String words;
         String[] string;
-        Scanner  scanner = new Scanner(System.in);
+        String option = "";
+        int num;
+        StringBuffer sb = new StringBuffer();
+        Scanner scanner = new Scanner(System.in);
         ArrayList<Integer> numbers = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            System.out.println("enter letter");
-            words= scanner.nextLine();
-            string = words.split("");
-            for (int j = 0; j < string.length; j++) {
-                int num = (int) (string[j].charAt(0));
-                numbers.add(num);
-            }
+        ArrayList<Integer> descending = new ArrayList<>();
+        System.out.println("ascending(a) or descending (d)");
+        option = (scanner.nextLine()).toUpperCase(Locale.ROOT);
+        System.out.println("enter word");
+        words = scanner.nextLine();
+        string = words.split("");
+        for (int j = 0; j < string.length; j++) {
+             num = string[j].charAt(0);
+             numbers.add(num);
         }
         int n = numbers.size();
         int temp = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 1; j < (n - i); j++) {
                 if (numbers.get((j - 1)) > numbers.get(j)) {
-                    //swap elements
-                    temp = numbers.get((j-1));
-                    numbers.set((j - 1),numbers.get(j));
-                    numbers.set(j,temp);
+                    temp = numbers.get((j - 1));
+                    numbers.set((j - 1), numbers.get(j));
+                    numbers.set(j, temp);
                 }
-
             }
         }
-        for (Integer number : numbers) {
-            System.out.println((char) numbers.get(number));
+        if (option.equals("D")) {
+            for (int i = 0; i < numbers.size(); i++) {
+                descending.add(numbers.get((numbers.size()-1) - i));
+            }
+            for (int i = 0; i < descending.size() ; i++){
+                int new_int = descending.get(i);
+                sb.append((char) new_int);
+            }
+        } else if (option.equals("A")) {
+            for (int i = 0; i < numbers.size(); i++) {
+                int new_int = numbers.get(i);
+                sb.append((char) new_int);
+            }
+
 
         }
+        String str = sb.toString();
+        System.out.println(str);
     }
 }
